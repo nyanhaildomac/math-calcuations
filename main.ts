679c0effc4918cde993e1dacb3c4d0c63b72f8ca
nyanhaildomac <113055210+nyanhaildomac@users.noreply.github.com>
2022-09-16 14:40:21 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onGesture(Gesture.Shake, function () {
+    let B = 0
+    let A = 0
+    num = A + 9
+    basic.showNumber(9)
+    num = B - 9
+    basic.showNumber(0)
+    num = B - 9
+    basic.showNumber(0)
+})
+let num = 0
+basic.showIcon(IconNames.Heart)
+num = 0
+basic.showNumber(0)
+basic.forever(function () {
+	
+})
